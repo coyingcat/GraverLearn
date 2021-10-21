b3df1dbff7f78cf67f7eaa91f3b247ff8665d5bc
邓江洲 <dengjiangzhouReal@gmail.com>
2021-10-21 19:51:08 +0800

    // UIView 的  func layoutSubviews()
        // 通过 CALayer 的 func layoutSublayers()
    
        // 调用起来

--- miniProj/LayerThing/LayerThing/CustomLayer.swift
+++ miniProj/LayerThing/LayerThing/CustomLayer.swift
@@ -12,8 +12,17 @@ class CustomLayer: CALayer {
     
     
     
+    // UIView 的  func layoutSubviews()
     
+    // 通过 CALayer 的 func layoutSublayers()
     
+    // 调用起来
+    override func layoutSublayers() {
+        super.layoutSublayers()
+        
+        
+        
+    }
     
     
     
---
+++ miniProj/LayerThing/LayerThing/CustomView.swift
@@ -0,0 +1,35 @@
+//
+//  CustomView.swift
+//  LayerThing
+//
+//  Created by Jz D on 2021/10/21.
+//
+
+import UIKit
+
+class CustomView: UIView {
+
+    
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        print("-    \(#function)    -")
+    }
+    
+
+    
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        
+    }
+    
+    
+}
+
+
+
+
+
+
