0322d6f57f62248d084af5123abfc53c2172b857
邓江洲 <dengjiangzhouReal@gmail.com>
2021-10-29 16:55:53 +0800

    ViewModel 不直接操作 Model
     通过一层间接
    
     Engine, 做一个桥接

--- proj_two/GraverDemo/Examples/List/homeList/Class/WMPoiListEngine.h
+++ proj_two/GraverDemo/Examples/List/homeList/Class/WMPoiListEngine.h
@@ -13,6 +13,11 @@ NS_ASSUME_NONNULL_BEGIN
 
 // 做网络请求的
 
+
+// 做数据驱动的
+
+// 加工 Model
+
 @interface WMPoiListEngine : WMGBaseEngine
 
 @end
@@ -22,4 +27,12 @@ NS_ASSUME_NONNULL_END
 
 
 
-
+/*
+ ViewModel 不直接操作 Model
+ 
+ 通过一层间接
+ 
+ Engine, 做一个桥接
+ 
+ 
+ */
